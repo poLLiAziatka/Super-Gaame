c71740c3f2eca51809b0e43c6744642e940dcd70
Polina838547 <62594984+poLLiAziatka@users.noreply.github.com>
2020-04-08 02:12:25 +0500

    intf + FPS, exit

--- interface.py
+++ interface.py
@@ -1,13 +1,17 @@
 import pygame
-
-pygame.init()
+import sys
 
 size = 50
+FPS = 30
 
+pygame.init()
 pygame.display.set_mode((size * 16, size * 9))
 """, pygame.FULLSCREEN"""
+clock = pygame.time.Clock()
 
-while 1:
+while True:
+    clock.tick(FPS)
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
