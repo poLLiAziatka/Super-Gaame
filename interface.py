637ade304776d113a894000c9be8688b140c026f
Polina838547 <62594984+poLLiAziatka@users.noreply.github.com>
2020-04-19 17:17:00 +0500

    something like the main menu

--- interface.py
+++ interface.py
@@ -1,17 +1,29 @@
 import pygame
-import sys
 
-size = 50
-FPS = 30
+FPS = 60
+size = 70
+
+background_color = (255, 255, 255)
+name_game_color = (255, 150, 100)
+start_button_color = (58, 202, 19)
+exit_button_color = (214, 44, 17)
 
 pygame.init()
-pygame.display.set_mode((size * 16, size * 9))
-""", pygame.FULLSCREEN"""
+
 clock = pygame.time.Clock()
 
-while True:
-    clock.tick(FPS)
+sc = pygame.display.set_mode((size * 16, size * 9))
+
+while 1:
+    sc.fill(background_color)
+
     for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        if i.type == pygame.QUIT: exit()
+
+    pygame.draw.rect(sc, name_game_color, (4 * size, 2 * size, 6 * size, 2 * size))
+    pygame.draw.rect(sc, start_button_color , (5 * size, 5 * size, 4 * size, 1 * size))
+    pygame.draw.rect(sc, exit_button_color, (5 * size, 6 * size, 4 * size, 1 * size))
+
+    pygame.display.update()
+
+    clock.tick(FPS)
