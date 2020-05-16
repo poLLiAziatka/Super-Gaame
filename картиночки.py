d68c6313bdb1b9dc95503dea5329e9cb1f66f0a6
alexandra <aleksandra_56@mail.ru>
2020-05-16 20:18:56 +0500

    + pictures

---
+++ картиночки.py
@@ -0,0 +1,25 @@
+from PIL import Image
+
+
+def Engineer():
+    return Image.open('1.jpg')
+
+
+def Leader_iron_sleeves():
+    return Image.open('2.jpg')
+
+
+def Katamaronov():
+    return Image.open('3.jpg')
+
+
+def Rosa_robot():
+    return Image.open('4.jpg')
+
+
+def Shershnyga():
+    return Image.open('5.jpg')
+
+
+def Journalist():
+    return Image.open('6.jpg')
